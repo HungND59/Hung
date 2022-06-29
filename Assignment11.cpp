9195f0d5dbe0107ba7d76f59e08c870b16834d84
Hungndlogin@gmail.com <Hungndlogin@gmail.com>
2022-06-29 20:18:37 +0700

    Modify Assignment11

--- Assignment11.cpp
+++ Assignment11.cpp
@@ -1,17 +1,52 @@
 #include "Assignment11.h"
+/*==========================================================================
+  FUNCTION   : CRectangle()
+  PARAMETERS : int input_width, int input_height
+  RETURN     : 
+  COMMENT    : The constructor function initializes the size of the rectangle
+==========================================================================*/
 CRectangle::CRectangle(int input_width, int input_height)
 {
     width = input_width;
     height = input_height;
 }
+/*==========================================================================
+  FUNCTION   : CLine()
+  PARAMETERS : int input_length
+  RETURN     :
+  COMMENT    : The constructor function initializes the size of the CLine
+==========================================================================*/
 CLine::CLine(int input_length)
 {
     length = input_length;
 }
+/*==========================================================================
+  FUNCTION   : CCircle()
+  PARAMETERS : int input_radium
+  RETURN     :
+  COMMENT    : The constructor function initializes the size of the CCircle
+==========================================================================*/
 CCircle::CCircle(int input_radium)
 {
     radium = input_radium;
 }
+/*==========================================================================
+  FUNCTION   : CSquare()
+  PARAMETERS : int length_side
+  RETURN     :
+  COMMENT    : The constructor function initializes the size of the CSquare
+==========================================================================*/
+CSquare::CSquare(int length_side)
+{
+    width = length_side;
+    height = length_side;
+}
+/*==========================================================================
+  FUNCTION   : draw()
+  PARAMETERS : int x, int y
+  RETURN     : void
+  COMMENT    : Function to draw a square
+==========================================================================*/
 void CRectangle::draw(int x, int y) {
     HPEN hPen;
     HWND console_handle = GetConsoleWindow();
@@ -21,6 +56,12 @@ void CRectangle::draw(int x, int y) {
     Rectangle(device_context, x, y, x + width, y + height);
     ReleaseDC(console_handle, device_context);
 }
+/*= ======================================================================== =
+FUNCTION   : draw()
+PARAMETERS : int x, int y
+RETURN : void
+COMMENT : Function to draw a Line
+==========================================================================*/
 void CLine::draw(int x, int y)
 {
     HPEN hPen;
@@ -28,8 +69,15 @@ void CLine::draw(int x, int y)
     HDC device_context = GetDC(console_handle);
     hPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
     SelectObject(device_context, hPen);
+    MoveToEx(device_context, x, y, (LPPOINT)NULL);
     LineTo(device_context, x + length, y);
 }
+/*= ======================================================================== =
+FUNCTION   : draw()
+PARAMETERS : int x, int y
+RETURN : void
+COMMENT : Function to draw a Circle
+==========================================================================*/
 void CCircle::draw(int x, int y)
 {
     HPEN hPen;
@@ -42,13 +90,30 @@ void CCircle::draw(int x, int y)
 int main() {
     int x;
     int y;
-    cout << "Nhap vao toa do hinh ve x va y:" << endl;
+    cout << "Enter the coordinates of the drawing on the console screen" << endl;
+    cout << "X = ";
     cin >> x;
+    cout << "Y = ";
     cin >> y;
-    CRectangle cRectangle(30,60);
-    CLine cLine(50);
-    CCircle cCircle(100);
-    cRectangle.draw(x, y);
-    cLine.draw(x, y);
-    cCircle.draw(x, y);
+    CPicture cPicture;
+    int Temp[4];
+    cPicture.Arr_ptr[0] = new CRectangle(30, 60);//initialize a Rectangle of width 30, height 60
+    cPicture.Arr_ptr[1] = new CLine(40);//initialize a Line of length 40
+    cPicture.Arr_ptr[2] = new CCircle(20);//initialize a Circle of radium 20
+    cPicture.Arr_ptr[3] = new CSquare(30);//initialize a Square of width 30, height 30
+    cout << "===================MENU=========================" << endl;
+    cout << "Enter the order to display 4 pictures in turn " << endl;
+    cout << "1: CRectangle " << endl;
+    cout << "2: CLine " << endl;
+    cout << "3: CCircle " << endl;
+    cout << "4: CSquare " << endl;
+    cout << "Example: 1234 " << endl;
+    for (int i = 0; i < 4; i++) {
+        cin >> Temp[i];
+    }
+    for (int i = 0; i < 4; i++ ) {
+        cPicture.Arr_ptr[(Temp[i])-1]->draw(x, y);
+        x = x + 100;
+    }
+    Sleep(5000);
 }
--- Assignment11.h
+++ Assignment11.h
@@ -6,31 +6,39 @@ class CShape {
 private:
     int color;
 public:
-    virtual void draw(int x, int y) = 0;
+    virtual void draw(int x, int y) = 0;//x, y are coordinates to draw the figure, passed in when calling the function
 };
 class CRectangle : public CShape {
 public:
     int width, height;
 public:
-    void draw(int x, int y);
-    CRectangle(int input_width, int input_height);
+    void draw(int x, int y);//Function to perform drawing
+    CRectangle() {};
+    CRectangle(int input_width, int input_height);//The constructor function initializes the size of the rectangle
 };
 class CLine : public CShape
 {
 private:
     int length;
 public:
-    void draw(int x, int y);
-    CLine(int length);
+    void draw(int x, int y);//Function to perform drawing
+    CLine(int length);//The constructor function initializes the size of the line
 };
 class CCircle : public CShape
 {
 private:
     int radium;
 public:
-    void draw(int x, int y);
-
- 
-
-    CCircle(int radium);
+    void draw(int x, int y);//Function to perform drawing
+    CCircle(int radium);//The constructor function initializes the size of the circle
+};
+class CSquare : public CRectangle
+{
+public:
+    CSquare(int side);//;//The constructor function initializes the size of the CSquare
+};
+class CPicture
+{
+public:
+    CShape* Arr_ptr[4];//Pointers are used to hold the addresses of 4 objects
 };
